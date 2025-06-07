473cefffa177b3da0a37c46f92b997e291fe3a5d
pop161516 <matasoc@gmail.com>
2025-06-07 18:29:25 +1000

    test

--- assignment3/script.js
+++ assignment3/script.js
@@ -1,20 +1,20 @@
-const section = document.querySelector("section.vid");
-const vid = section.querySelector("video");
+// const section = document.querySelector("section.vid");
+// const vid = section.querySelector("video");
 
-vid.pause();
+// vid.pause();
 
-const scroll = () => {
-  const distance = window.scrollY - section.offsetTop;
-  const total = section.clientHeight - window.innerHeight;
+// const scroll = () => {
+//   const distance = window.scrollY - section.offsetTop;
+//   const total = section.clientHeight - window.innerHeight;
 
-  let percentage = distance / total;
-  percentage = Math.max(0, percentage);
-  percentage = Math.min(percentage, 1);
+//   let percentage = distance / total;
+//   percentage = Math.max(0, percentage);
+//   percentage = Math.min(percentage, 1);
 
-  if (vid.duration > 0) {
-    vid.currentTime = vid.duration * percentage;
-  }
-};
+//   if (vid.duration > 0) {
+//     vid.currentTime = vid.duration * percentage;
+//   }
+// };
 
-scroll();
-window.addEventListener("scroll", scroll);
+// scroll();
+// window.addEventListener("scroll", scroll);
